67f391a8ae35a8c09489b7767cbebc326ed8f760
Zhanna_Mukhanova <mail@ispolina.art>
2022-11-27 22:03:49 +0300

    Fourth task

---
+++ lesson1_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine($"Четные числа до {N}:");
+while (i <= N)
+{
+    if ( i % 2 == 0)
+    {
+        Console.WriteLine($"{i} ");
+    }
+    
+    i=i+1;
+}
