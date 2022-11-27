f57fbd4d5f342ed817f38b013a129c1d6b21715a
Zhanna_Mukhanova <mail@ispolina.art>
2022-11-27 21:31:40 +0300

    Second task

---
+++ lesson1_1/Program1.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите число A"); 
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B"); 
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+int min = number2;
+
+
+if (max < number2) 
+{
+    max = number2;
+    min = number1;
+    Console.Write("max =");
+    Console.WriteLine(max );
+
+    Console.Write("min =");
+    Console.WriteLine( min );
+
+}
+
+else if (max > number2)
+{
+    max = number1;
+    min = number2;
+    Console.Write("max =");
+    Console.WriteLine(max );
+
+    Console.Write("min =");
+    Console.WriteLine( min );
+
+}
+else if (number1 == number2)
+{
+    Console.WriteLine("Числа одинаковые");
+}
+
---
+++ lesson1_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A") ; 
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B") ; 
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C") ; 
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int MAX = number1;
+
+if (number1 > MAX) MAX = number1;
+if (number2 > MAX) MAX = number2;
+if (number3 > MAX) MAX = number3;
+
+Console.Write("MAX = ");
+Console.WriteLine(MAX);
