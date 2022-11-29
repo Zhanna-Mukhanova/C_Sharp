2b95620b7ee2efd0261e18a23351f5ef91c63634
Zhanna_Mukhanova <mail@ispolina.art>
2022-11-29 23:32:44 +0300

    Lesson two. Exercise two

---
+++ HomeWork/lesson2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого числа.
+// 645 -> 65
+// 782 -> 72
+// 918 -> 98
+
+int TakeNum(int num)
+{   
+    Console.WriteLine(num);
+    return (((num / 100)*10)+(num % 10));
+}
+
+Console.WriteLine(TakeNum(new Random().Next(100, 1000)));
