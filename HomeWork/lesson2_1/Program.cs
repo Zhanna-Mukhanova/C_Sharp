2cfa996f5a46b65358cec65d91574027ef8137ee
Zhanna_Mukhanova <mail@ispolina.art>
2022-11-29 23:21:06 +0300

    Lesson two. Exercise one

--- HomeWork/lesson2_1/Program.cs
+++ HomeWork/lesson2_1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int TakeNum(int num)
+{   
+    Console.WriteLine(num);
+    return num % 100 / 10;
+}
+
+int a = TakeNum(new Random().Next(100, 1000));
+Console.WriteLine(a);
+
+
+
+
