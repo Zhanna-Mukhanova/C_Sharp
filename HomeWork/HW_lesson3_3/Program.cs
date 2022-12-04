228da3e7e2400b27da953903d24839c58fda2b87
Zhanna_Mukhanova <mail@ispolina.art>
2022-12-04 20:08:37 +0300

    Lesson three. Exercise Three

---
+++ HomeWork/HW_lesson3_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Koub(int N)
+{
+    int b = 0;
+    
+    while (b < N)
+    {
+        double Coube = Math.Pow((b + 1), 3);
+        Console.Write($" {Coube} ");
+        b = b + 1;
+    }
+    Console.WriteLine();
+}
+ Koub(5);
+
+ Koub(50);
+
+ Koub(3);
+
+ Koub(30);
