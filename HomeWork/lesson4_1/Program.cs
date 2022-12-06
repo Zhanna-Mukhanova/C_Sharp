8fcbad4134c3aa02d8ef95b9f254f13bb0e81214
Zhanna_Mukhanova <mail@ispolina.art>
2022-12-06 10:40:18 +0300

    Lesson three. Classwork2

---
+++ HomeWork/lesson4_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество чисел в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int NumbCount(int n)
+{
+    if(n == 0)
+    {
+        return(1);
+    }
+    int count = 0;
+    while (n > 0)
+{
+    n = n/10;
+    count++;
+}
+return count;
+
+}
+Console.WriteLine(NumbCount(int.Parse(Console.ReadLine())));
