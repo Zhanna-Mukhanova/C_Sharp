f62fcff03897cb3a23ea5c563e58e585154a8764
Zhanna_Mukhanova <mail@ispolina.art>
2022-12-07 22:37:05 +0300

    Lesson four. HW 4.1"

---
+++ HomeWork/HW_4_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B. Без модуля Math, используем цикл for.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double PowNun(int a, int b)
+{
+    double result = 1;
+    if (a < 0 || b < 0)
+    return 0;
+    
+    for (int i = 1; i <= b; i++)
+    {
+            result = result* a;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число");
+int num_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int num_2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(PowNun(num_1, num_2));
