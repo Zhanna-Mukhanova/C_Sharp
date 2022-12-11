808931bdc1d63b7337a31ac180e212601181d4db
Zhanna_Mukhanova <mail@ispolina.art>
2022-12-11 21:00:32 +0300

    Lesson five. HW 5.2

---
+++ HomeWork/HW_5_2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 26
+[-4, -6, 4, 6] -> 0
+*/
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        int random = new Random().Next(100, 999);
+        array[i] = random;
+        Console.Write($"{random} ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+int SumOddNumbers(int[] array)
+{
+    int size = array.Length;
+    int summa = 0; 
+    for(int i = 0; i < size; i = i + 2)
+    {
+        summa = summa + array[i];
+    }
+
+    return summa;
+}
+
+Console.WriteLine(SumOddNumbers(CreateArray(4)));
